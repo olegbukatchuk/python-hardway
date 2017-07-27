c960c93e511f30864012349d85304d9449718b55
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-07-27 17:12:23 +0300

    ok

---
+++ ex11.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+print u"Сколько тебе лет?",
+age = raw_input()
+print u"Каков твой рост?",
+height = raw_input()
+print u"Сколько ты весишь?"
+weight = raw_input()
+
+print u"Итак, тебе %r лет, в тебе %r см роста и %r кг веса." % (age, height, weight)
