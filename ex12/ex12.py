37448602c1bb20a5cfd36c7062c828322923ad57
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-07-28 13:52:56 +0300

    ok

--- ex12/ex12.py
+++ ex12/ex12.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+import codecs, sys
+outf = codecs.getwriter('utf-8')(sys.stdout, errors='replace')
+sys.stdout = outf
+
+age = raw_input(u"Сколько тебе лет? ")
+height = raw_input(u"Каков твой рост? ")
+weight = raw_input(u"Сколько ты весишь? ")
+
+print u"Итак, тебе %r лет, в тебе %r см роста и %r кг веса." % (
+    age, height, weight)
